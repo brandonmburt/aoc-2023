3801b3edb23fa4a976beca34e8f522a04351a74c
Brandon <brandon.michael.burt@gmail.com>
2023-12-07 16:21:40 -0500

    Day 2 refactor

--- src/puzzles/day-02.ts
+++ src/puzzles/day-02.ts
@@ -2,29 +2,22 @@
 
 export function solvePuzzle2(input: string): [number, number] {
 
-    let p1 = 0, p2 = 0;
+    const shouldInvalidate = (color: string, n: number): boolean => {
+        return (color === 'red' && n > 12) || (color === 'green' && n > 13) || (color === 'blue' && n > 14);
+    }
 
+    let p1 = 0, p2 = 0;
     input.split('\n').forEach((line) => {
-        let isValid = true; // p1
-        let minRed = 0, minGreen = 0, minBlue = 0; // p2
+        let isValid = true, minRed = 0, minGreen = 0, minBlue = 0;
         const [game, subsets] = line.split(': ');
         const id = +game.split(' ')[1];
         subsets.split('; ').forEach((subset) => {
             subset.split(', ').forEach(x => {
                 const [quantity, color] = x.split(' ');
-                if ((color === 'red' && +quantity > 12) ||
-                    (color === 'green' && +quantity > 13) ||
-                    (color === 'blue' && +quantity > 14)) {
-                        isValid = false;
-                }
-
-                if (color === 'red') {
-                    minRed = Math.max(minRed, +quantity);
-                } else if (color === 'green') {
-                    minGreen = Math.max(minGreen, +quantity);
-                } else if (color === 'blue') {
-                    minBlue = Math.max(minBlue, +quantity);
-                }
+                if (shouldInvalidate(color, +quantity)) isValid = false;
+                if (color === 'red') minRed = Math.max(minRed, +quantity);
+                else if (color === 'green') minGreen = Math.max(minGreen, +quantity);
+                else if (color === 'blue') minBlue = Math.max(minBlue, +quantity);
             });
         });
         if (isValid) p1 += id;
